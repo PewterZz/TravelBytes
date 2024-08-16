a7757dadb6e7725709facdebe803a7d22fee8797
Peter Nelson Subrata <peterns1609@gmail.com>
2024-08-16 19:05:46 +1000

    [ADD] Trying to add the custom layout, animations for logo is done just need the after effects.

---
+++ TravelBytes/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TravelBytes
+//
+//  Created by Peter Subrata on 16/8/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ TravelBytes/Views/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  TravelBytes
+//
+//  Created by Peter Subrata on 16/8/2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
