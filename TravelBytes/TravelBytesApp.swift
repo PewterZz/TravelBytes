ecc36bcea852fd1fb042b8fc2fe1a4231d17c5b0
Peter Nelson Subrata <peterns1609@gmail.com>
2024-08-16 13:07:35 +1000

    Initial Commit

---
+++ TravelBytes/TravelBytesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBytesApp.swift
+//  TravelBytes
+//
+//  Created by Peter Subrata on 16/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelBytesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
