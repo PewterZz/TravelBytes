a2cc23f860d9197881d1f91d9998f62c614c47ec
Peter Nelson Subrata <peterns1609@gmail.com>
2024-08-23 19:15:57 +1000

    [ADD] Onboarding Page and Better Structure

---
+++ TravelBytes/Extensions/Color+hex.swift
@@ -0,0 +1,8 @@
+//
+//  Color+hex.swift
+//  TravelBytes
+//
+//  Created by Peter Subrata on 21/8/2024.
+//
+
+import Foundation
---
+++ TravelBytes/Views/TabsView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  TravelBytes
+//
+//  Created by Peter Subrata on 21/8/2024.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
